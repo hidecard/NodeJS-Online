5b5c3d2530d6024b3156de6c19963b4e6fd788ba
hidecard <hidecard1500@gmail.com>
2025-07-24 21:35:06 +0630

    first commit

---
+++ day1.js
@@ -0,0 +1,23 @@
+// var name = "Mg Mg";
+
+// console.log("Hello World");
+// console.log(name);
+
+// setTimeout
+// setTimeout(function () {
+//     console.log("Hello World");
+// }, 1000);
+
+
+// setInterval
+
+// setInterval(function () {
+//     console.log("Hello World");
+// }, 1000);
+
+
+// 
+
+
+
+
